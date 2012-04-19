17f6ecdb5d68e02db24e9663c483def114852a87
John Purcell <locke.renard@gmail.com>
2012-04-19 16:36:54 -0600

    Updated RAWCRUNCH to be more expandable
    Removed hash tables of rawmaterial ID's
    was: $MaterialType{ID}="String Name"
    now: reads ID/name into individual hash tables
    $rawprice{ID}=price
    $names{ID}=name
    
    Can remove split('i', $key).

--- costcrunch.pl
+++ costcrunch.pl
@@ -24,6 +24,10 @@ my %rawprice;#raw material prices
 my %comp; #Component hash
 ## ID{comp}=cost
 
+my %names;
+##Saves ID name
+## name{ID}="string"
+
 my $RAM; #All RAM Cost the same (if exists %ramID; $quant * $RAM)
 my %ramID=(
 	11476, "Ammo Tech",
@@ -64,85 +68,24 @@ my $setupT=time;
 print "\nSub-component setup time: ".($tstart-$setupT)."\n";
 
 
-sub RawCrunch{ #Loads %rawprice
-	#{Hash initializers
-	my %component=(
-	16670, "Crystalline Carbonite",
-	17317, "Fermionic Condensates",
-	16673, "Fernite Carbide",
-	16683, "Ferrogel",
-	16679, "Fullerides",
-	16682, "Hypersynaptic Fibers",
-	16681, "Nanotransisotrs",
-	16680, "Phenolic Composits",
-	16678, "Sylramic Fibers",
-	16671, "Titanium Carbonite",
-	16672, "Tungsten Carbonite",
+sub RawCrunch{ #Loads %rawprice	
+	my %skipString=(
+		"source", 0,
+		"fresh",0,
 	);
-	
-	my %datacore=(
-	20417, "Datacore - Electromagnetic Physics",
-	20418, "Datacore - Electronic Engineering",
-	20419, "Datacore - Graviton Physics",
-	20411, "Datacore - High Energy Physics",
-	20171, "Datacore - Hydromagnetic Physics",
-	20413, "Datacore - Laser Physics",
-	20424, "Datacore - Mechanical Engineering",
-	20415, "Datacore - Molecular Engineering",
-	20416, "Datacore - Nanite Engineering",
-	20423, "Datacore - Nuclear Physics",
-	20412, "Datacore - Plasma Physics",
-	20414, "Datacore - Quantum Physics",
-	20420, "Datacore - Rocket Science",
-	20421, "Datacore - Amarrian Starship Engineering",
-	25887, "Datacore - Caldari Starship Engineering",
-	20410, "Datacore - Gallentean Starship Engineering",
-	20172, "Datacore - Minmatar Starship Engineering",
-	);
-	
-	my %mineral=(
-	11399, "Morphite",
-	37, "Isogen",
-	40, "Megacyte",
-	36, "Mexallon",
-	38, "Nocxium",
-	35, "Pyerite",
-	34, "Tritanium",
-	39, "Zydrine",
-	);
-
-	my %PI=(
-	3689, "Mechanical Parts",
-	9842, "Miniature Electronics",
-	9834, "Guidance System",
-	9848, "Robotics",
-	9830, "Rocket Fuel",
-	9838, "Super Conductors",
-	9840, "Transmitter",
-	3828, "Construction Blocks",
-	3685, "Hydrogen Batteries",
-	3687, "Electronic Parts",
-	);
-	#}
-	
-	foreach my $raw1Key (keys %component){
-		my $itemID="i".$raw1Key;
-		$rawprice{$raw1Key}= $pricesheet->{component}->{$itemID}->{$pricekey};
+		
+	foreach my $matType (keys %{$pricesheet}){
+		if (exists $skipString{$matType} ){
+			next;
+		}
+		my $product;
+		foreach my $iproduct (keys %{$pricesheet->{$matType}}){
+			(undef, $product) = split ('i', $iproduct);
+			
+			$rawprice{$product} = $pricesheet->{$matType}->{$iproduct}->{$pricekey};
+			$names{$product} = $pricesheet->{$matType}->{$iproduct}->{name};
+		}
 	}
-	
-	foreach my $raw2Key (keys %mineral){
-		my $itemID="i".$raw2Key;
-		$rawprice{$raw2Key}= $pricesheet->{mineral}->{$itemID}->{$pricekey};
-	}
-	
-	foreach my $raw3Key (keys %datacore){
-		my $itemID="i".$raw3Key;
-		$rawprice{$raw3Key}= $pricesheet->{datacore}->{$itemID}->{$pricekey};
-	}
-	
-	#foreach my $raw4Key (keys %PI){
-	#	$rawprice{$raw4Key}= $pricesheet->{PI}->{$raw4Key}->{$pricekey};
-	#}
 	print "Raw Materials Loaded\n";
 };
 
@@ -153,20 +96,15 @@ sub CompCrunch{ #Loads %comp, %capital, and sets $RAM
 		foreach my $compKey (keys %{$componentsheet->{component}->{$typeKey}}){
 			(undef, $itemid)=split('i', $compKey);
 			$comp{$itemid}=0;
-			#print $componentsheet->{component}->{$typeKey}->{$compKey}->{name}." ";
+			$names{$itemid}=$componentsheet->{component}->{$typeKey}->{$compKey}->{name};
+			
 			foreach my $gooKey (keys %{$componentsheet->{component}->{$typeKey}->{$compKey}}){
 				if ($gooKey =~ m/\s*i[0-9]/){#if i### use for component calc
 					(undef, $compid)=split('i', $gooKey);
-					#$compid=$gooKey;
-					
-					#print $compid."x".$componentsheet->{component}->{$typeKey}->{$compKey}->{$gooKey}->{content}." ";
+										
 					$comp{$itemid}= $comp{$itemid}+($componentsheet->{component}->{$typeKey}->{$compKey}->{$gooKey}->{content})*($rawprice{$compid});
-					#print $gooKey."x".$componentsheet->{component}->{$typeKey}->{$compKey}->{$gooKey}->{content}." ";
 				}
 			}
-			#print "cost:".$comp{$itemid};
-			#print "key:".$itemid." price:".$comp{$itemid}."\n";
-			
 		}
 	}
 	
@@ -175,15 +113,15 @@ sub CompCrunch{ #Loads %comp, %capital, and sets $RAM
 	foreach my $typeKey2 (keys %{$componentsheet->{capital}}){
 		(undef, $itemid) = split ('i', $typeKey2);
 		$capital{$itemid}=0;
-		#print $componentsheet->{capital}->{$typeKey2}->{name}.": ";
+		$names{$itemid}=$componentsheet->{capital}->{$typeKey2}->{name};
+
 		foreach my $Cpart (keys %{$componentsheet->{capital}->{$typeKey2}}){
 			if ($Cpart =~ m/\s*i[0-9]/){
 				(undef, $compid)=split('i', $Cpart);
 				$capital{$itemid}+= $componentsheet->{capital}->{$typeKey2}->{$Cpart}->{content} * $rawprice{$compid};
-				#print $compid."x".($componentsheet->{capital}->{$typeKey2}->{$Cpart}->{content})." ";
+				
 			}
 		}
-		#print $capital{$itemid}."\n";
 	}
 	
 	print "Capital component prices calculated\n";
@@ -193,7 +131,7 @@ sub CompCrunch{ #Loads %comp, %capital, and sets $RAM
 		my $Rmin;
 		if ($Rpart =~ m/\s*i[0-9]/){
 			(undef, $Rmin) = split ('i', $Rpart);
-			$RAM += $componentsheet->{RAM}->{RAM}->$Rpart->{content} * $rawprice{$Rmin};
+			$RAM += $componentsheet->{RAM}->{RAM}->{$Rpart}->{content} * $rawprice{$Rmin};
 		}
 	}
 	
