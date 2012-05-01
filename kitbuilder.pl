81b4adada4553f94fdee854ee6a22b9f2c787a51
John Purcell <locke.renard@gmail.com>
2012-05-01 14:55:24 -0600

    working draft of kitbuilder
    -quickKits works, but not validated
    -kits.xml works but not validated
    
    ISSUES
    -printer: printing crap?
    -Validate output

--- kitbuilder.pl
+++ kitbuilder.pl
@@ -30,6 +30,7 @@ my %kits;	#$kits{$inventor}{itemID}=quantity
 
 my $joblist="producers.xml";
 my $matlist="manufacture.xml";
+my $outfile="kits.xml";
 
 my $staffpage = new XML::Simple;
 my $staff = $staffpage->XMLin($joblist);
@@ -41,31 +42,39 @@ my $mats = $matspage->XMLin($matlist);
 
 &quickKits;
 
+&printer;
+
 sub loadKits{##using slow search method
-	foreach my $workers (keys %{$staff}){
-		print $workers;
-		$employee{$workers}=$staff->{$workers}->{name};
-		foreach my $items (keys %{$staff->{$workers}}){
-			my $mult=0;
-			my $q=$staff->{$workers}->{$items}->{content};
-			$names{$items}={$workers}->{$items}->{name};
-			if ($items eq "name"){
+	foreach my $workers (keys %{$staff->{staff}}){
+		$employee{$workers}=$staff->{staff}->{$workers}->{name};
+		print $employee{$workers}.":";
+		
+		foreach my $products (keys %{$staff->{staff}->{$workers}}){
+			if ($products eq "name"){
 				next;
 			}
-			foreach my $class (keys %{$mats}){
-				foreach my $type (keys %{$mats->{$class}}){
-					foreach my $itemID (keys %{$mats->{$class}->{$type}}){
-						if ($itemID eq $items){
-							$mult=$q/($mats->{$class}->{$type}->{$itemID}->{qty});
-							
-							foreach my $parts (keys %{$mats->{$class}->{$type}->{$itemID}}){
+			$names{$products}=$staff->{staff}->{$workers}->{$products}->{name};
+			my $qty = $staff->{staff}->{$workers}->{$products}->{content};
+			my $mult=0;
+			#print " ".$names{$products}."x".$qty;
+			foreach my $class(keys %{$mats}){
+				if ($class eq "capital"){
+					next; #make sub for capital kit (all minerals)
+				}
+				foreach my $group(keys %{$mats->{$class}}){
+					foreach my $itemID(keys %{$mats->{$class}->{$group}}){
+						if ($itemID eq $products){
+							$names{$itemID}=$mats->{$class}->{$group}->{$itemID}->{name};
+							$mult=$mats->{$class}->{$group}->{$itemID}->{qty};
+							my $x=$qty/$mult;
+							foreach my $parts(keys %{$mats->{$class}->{$group}->{$itemID}}){
 								if ($parts =~ /i/ or $parts =~ /[A-Z]/){
-									$names{$parts}=$mats->{$class}->{$type}->{$itemID}->{$parts}->{name};
-									if (!(exists $kits{$workers}{$parts})){
-										$kits{$workers}{$parts}=$mats->{$class}->{$type}->{$itemID}->{$parts}->{content} * $mult;
+									$names{$parts}=$mats->{$class}->{$group}->{$itemID}->{$parts}->{name};
+									if (exists $kits{$workers}{$parts}){
+										$kits{$workers}{$parts}+= $mats->{$class}->{$group}->{$itemID}->{$parts}->{content}*$x;
 									}
 									else{
-										$kits{$workers}{$parts}+=$mats->{$class}->{$type}->{$itemID}->{$parts}->{content} * $mult;
+										$kits{$workers}{$parts}= $mats->{$class}->{$group}->{$itemID}->{$parts}->{content}*$x
 									}
 								}
 							}
@@ -73,38 +82,49 @@ sub loadKits{##using slow search method
 					}
 				}
 			}
+			print Dumper (%kits);
 		}
+		print "\n";
 	}
-	print Dumper{%kits};
 };
 
 sub quickKits{
 	&typeLoader();
 	
-	foreach my $pilot (keys %{$staff}){
-		print $pilot;
+	foreach my $pilot (keys %{$staff->{staff}}){
 		$employee{$pilot}=$staff->{$pilot}->{name};
-		foreach my $produce (keys %{$staff->{$pilot}}){
-			if ($produce eq "name"){
+		
+		foreach my $products (keys %{$staff->{staff}->{$pilot}}){
+			if ($products eq "name"){
 				next;
 			}
 			
-			$names{$produce}=$mats->{T2}->{($subset{$produce})}->{$produce}->{qty};
-			my $mult = ($staff->{$pilot}->{$produce}->{content})/($mats->{T2}->{($subset{$produce})}->{$produce}->{qty});
-			foreach my $parts (keys %{$mats->{T2}->{($subset{$produce})}->{$produce}}){
+			$names{$products}=$staff->{staff}->{$pilot}->{$products}->{name};
+			my $qty = $staff->{staff}->{$pilot}->{$products}->{content};
+			
+			my $div = $mats->{T2}->{($subset{$products})}->{$products}->{qty};
+			
+			my $x = $qty/$div;
+			print $names{$products}."x".$x."\n";
+			foreach my $parts (keys %{$mats->{T2}->{($subset{$products})}->{$products}}){
+				if ($parts eq "bld_time"){
+					next;
+				}
 				if ($parts =~ /i/ or $parts =~ /[A-Z]/){
-					if (exists $kits{$pilot}{$produce}{$parts}){
-						$kits{$pilot}{$produce}{$parts} += $mats->{T2}->{($subset{$produce})}->{$produce}->{$parts}->{content} * $mult;
+					$names{$parts}=$mats->{T2}->{($subset{$products})}->{$products}->{$parts}->{name};
+					#print $names{$parts}."x".
+					if (exists $kits{$pilot}{$parts}){
+						$kits{$pilot}{$parts}+=$mats->{T2}->{($subset{$products})}->{$products}->{$parts}->{content} * $x;
 					}
 					else{
-						$kits{$pilot}{$produce}{$parts} = $mats->{T2}->{($subset{$produce})}->{$produce}->{$parts}->{content} * $mult;
+						$kits{$pilot}{$parts}=$mats->{T2}->{($subset{$products})}->{$products}->{$parts}->{content} * $x;
 					}
-					$names{$parts}=$mats->{T2}->{($subset{$produce})}->{$produce}->{$parts}->{name};
 				}
-
 			}
 		}
+		#print Dumper(%kits);
 	}
+	
 };
 
 sub typeLoader{
@@ -113,6 +133,29 @@ sub typeLoader{
 			$subset{$item}=$type;
 		}
 	}
+	#print Dumper(%subset);
 
+};
+
+sub printer{
+	my $writeout = new IO::File (">$outfile");
+	
+	my $writer = new XML::Writer ( DATA_MODE => 'true', DATA_INDENT => 2, OUTPUT => $writeout);
+	
+	$writer->xmlDecl( 'UTF-8' );
+	
+	$writer->startTag('root');
+	
+	foreach my $people (keys %{$staff->{staff}}){
+		$writer->startTag ($people, 'name'=>$employee{$people});
+		foreach my $parts (keys %{$kits{$people}}){
+			$writer->startTag($parts, 'name'=>$names{$parts});
+			$writer->characters ($kits{$people}{$parts});
+			$writer->endTag();
+		}
+		$writer->endTag();
+	}
+	$writer->endTag();
+	$writer->end();
 };
 
