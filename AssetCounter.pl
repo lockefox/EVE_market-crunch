ad5cf558d412e4a0a8527bfe717e932b3b803390
John Purcell <locke.renard@gmail.com>
2012-06-17 23:04:28 -0600

    Updated skip list on Asset cruncher and committed kit changes

--- AssetCounter.pl
+++ AssetCounter.pl
@@ -38,7 +38,7 @@ my $shoplist = "kits.xml";
 my %systems = (
 	30000142,"Jita",
 	30005310,"Scheenins",
-	30005310,"Dodixie",
+	30002659,"Dodixie",
 	30005297,"Ouelletta",
 );
 
@@ -66,18 +66,42 @@ my %inSpace = (
 	1005461047619,"Comp Lab IV",
 );
 
+my %skip = (
+	16216,"Moblie Lab",	#Mobile Laboratory
+	13780,"Equipment Assembly";
+	16213,"CL Tower",
+	24654,"Med Ship Array",
+	28351,"Adv Lab",
+	20061,"CM Tower",
+	24660,"Component Array",
+	17185,"EXP array",
+	17184,"KIN array",
+	17187,"EM array",
+	17186,"THRM array",
+	24659,"Drone array",
+	
+);
+###FLAGS
+#	62=delivery?
+#	116=
+#	120=production
+#	119=
+#	121=Directors
+#	ID=27<--office
 
 #####	XML Objects   #####
 
 #my $cAPI_Assets = new XML::Simple;
 #my $AssetList = $cAPI_Assets->XMLin(get($evesite.$cAssets."vCode=".$corp_VCODE."&charID=".$charid."&keyID=".$corp_ID));
+my $cAPI_Assets = new XML::Simple;
+my $AssetList = $cAPI_Assets->XMLin("AssetList.xml");
 
 my $local_Shopping = new XML::Simple;
 my $ShoppingList = $local_Shopping->XMLin($shoplist);
 
 #####	Globals   #####
-my %datacores;
-my %minerals;
+my %datacore;
+my %mineral;
 my %component;
 my %goo;
 my %PI;
@@ -107,18 +131,18 @@ sub globalLoader{
 	}
 	foreach my $dcKey (keys %{$ShoppingList->{Shopping}->{Datacore}}){
 		$names{$ShoppingList->{Shopping}->{Datacore}->{$dcKey}->{id}}=$dcKey;
-		$component{$ShoppingList->{Shopping}->{Datacore}->{$dcKey}->{id}}=$ShoppingList->{Shopping}->{Datacore}->{$dcKey}->{content};
+		$datacore{$ShoppingList->{Shopping}->{Datacore}->{$dcKey}->{id}}=$ShoppingList->{Shopping}->{Datacore}->{$dcKey}->{content};
 	}
 	foreach my $gooKey (keys %{$ShoppingList->{Shopping}->{goo}}){
 		$names{$ShoppingList->{Shopping}->{goo}->{$gooKey}->{id}}=$gooKey;
-		$component{$ShoppingList->{Shopping}->{goo}->{$gooKey}->{id}}=$ShoppingList->{Shopping}->{goo}->{$gooKey}->{content};
+		$goo{$ShoppingList->{Shopping}->{goo}->{$gooKey}->{id}}=$ShoppingList->{Shopping}->{goo}->{$gooKey}->{content};
 	}
 	foreach my $minKey (keys %{$ShoppingList->{Shopping}->{Mineral}}){
 		$names{$ShoppingList->{Shopping}->{Mineral}->{$minKey}->{id}}=$minKey;
-		$component{$ShoppingList->{Shopping}->{Mineral}->{$minKey}->{id}}=$ShoppingList->{Shopping}->{Mineral}->{$minKey}->{content};
+		$mineral{$ShoppingList->{Shopping}->{Mineral}->{$minKey}->{id}}=$ShoppingList->{Shopping}->{Mineral}->{$minKey}->{content};
 	}
 	foreach my $PIKey (keys %{$ShoppingList->{Shopping}->{PI}}){
 		$names{$ShoppingList->{Shopping}->{PI}->{$PIKey}->{id}}=$PIKey;
-		$component{$ShoppingList->{Shopping}->{PI}->{$PIKey}->{id}}=$ShoppingList->{Shopping}->{PI}->{$PIKey}->{content};
+		$PI{$ShoppingList->{Shopping}->{PI}->{$PIKey}->{id}}=$ShoppingList->{Shopping}->{PI}->{$PIKey}->{content};
 	}
 }
